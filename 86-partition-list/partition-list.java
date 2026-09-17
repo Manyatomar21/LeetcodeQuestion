94b50307bfea0e96d4c6ea39662f44b27899e511
Manyatomar21 <manyatomar21@gmail.com>
2026-09-17 21:26:58 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (13.64%) - LeetSync

---
+++ 86-partition-list/partition-list.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        ListNode smaller= new ListNode(0);
+        ListNode larger= new ListNode(0);
+        ListNode small= smaller;
+        ListNode large= larger;
+        ListNode initial= head;
+        while(initial!=null){
+            if(initial.val<x){
+                small.next= initial;
+                small= small.next;
+            } else{
+                large.next= initial;
+                large= large.next;
+            }
+            initial= initial.next;
+        }
+        large.next= null;
+    small.next= larger.next;
+    return smaller.next;
+}
+}
